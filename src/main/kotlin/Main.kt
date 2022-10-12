8bdb7f16cc35502b107e74c22ff32fe2ae3a8890
Raympel <pivkin97@mail.ru>
2022-10-12 19:40:35 +0300

    Working with the alphabet

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,15 @@
+
+fun main(args: Array<String>) {
+
+    var collection: MutableList<Any> = mutableListOf()
+
+    for(el in 'a'..'z')
+        collection.add(el)
+
+    println(collection)
+
+}
+
+
+
+
